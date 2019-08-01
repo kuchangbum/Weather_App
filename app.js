b9c490d1862e580fac4f4a181d4e0c212635a1c0
kuchangbum <50977342+kuchangbum@users.noreply.github.com>
2019-08-01 20:54:34 +0900

    ver3
    기존의 틀과 날씨API 대입

--- app.js
+++ app.js
@@ -1,49 +1,94 @@
-window.addEventListener("load", () => {
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector(
-        ".temperature-description"
-    );
-    let temperatureDegree = document.querySelector(".temperature-degree");
-    let locationTimezone = document.querySelector(".location-timezone");
-    let temperatureSection = document.querySelector(".temperature-section");
-    const temperatureSpan = document.querySelector("temperature-section span");
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+// api key : 82005d27a116c2880c8f0fcb866998a0
 
-            const proxy = "https://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}
-            https://api.darksky.net/forecast/7c00f474ab3eda052d7303bc7d0f28bd/${lat},${long}`;
+// SELECT ELEMENTS
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
 
-            fetch(api)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    const { temperature, summary, icon } = data.currently;
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = summary;
-                    locationTimezone.textContent = date.timezone;
-                     //Formula for celsius
-                     let celsius = (temperature - 32) * (5/9);
-                    //Set Icon
-                    setIcons(icon, document.querySelector(".icon"));
+// App data
+const weather = {};
 
-                    //Change temperature to Celsius/Farenheit
-                    temperatureSection.addEventListener("click", () => {
-                        if (temperatureSpan.textContent === "F") {
-                            temperatureSpan.textContent = "C";
-                            temperatureDegree.textContent = Math.floor(celsius);
-                        } else {
-                            temperatureSpan.textContent = "F";
-                            temperatureDegree.textContent = temperature;
-                        }
-                    });
-                });
+weather.temperature = {
+    unit : "celsius"
+}
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
         });
-    }
+}
 
-})
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
+
